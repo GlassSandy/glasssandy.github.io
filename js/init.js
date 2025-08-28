eae68ebccb7a5273818cfd9805d58c9def2c6c30
GlassSandy <semifiremode@gmail.com>
2025-08-28 16:23:18 +0300

    big update cookie warning all pages / menu fix / hovers / minor fixes

--- js/init.js
+++ js/init.js
@@ -5,3 +5,8 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("currentYear").textContent = new Date().getFullYear();
+});
